801b8f56358af8f4ba530a3f463915790f65e260
Sadullo <sadulloburiyev1992@gmail.com>
2024-08-04 22:48:30 +0500

    feat: add api gateway link to cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://moh7jtvle7.execute-api.eu-north-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://p3euc2ftbkhjpkq3tp6ybtmno40rcdxr.lambda-url.eu-north-1.on.aws",
+  cart: "https://s28nswt1pl.execute-api.eu-north-1.amazonaws.com",
 };
 
 export default API_PATHS;
